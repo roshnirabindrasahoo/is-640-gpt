b9273dd993e180c22243784e7a252eec2ef99781
Bhavya Samtani <bhavya.samtani01@student.csulb.edu>
2024-12-09 12:36:01 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,54 +1,50 @@
 import torch
-from model import BigramLanguageModel
-from data import load_data, split_data, get_batch
 
-batch_size = 16
-block_size = 8
-max_iters = 100
-eval_interval = 500
-learning_rate = 0.01
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 50
-n_embd = 32
-n_head = 4
-n_layer = 3
-dropout = 0.2
+#defining class Trainer
+#adam optimizer
+class Trainer:
+    def __init__(self, model, data_obj, max_iters=5000, eval_interval=500, eval_iters=200, batch_size=64, learning_rate=3e-4):
+        self.model = model
+        self.data = data_obj
+        self.max_iters = max_iters
+        self.eval_interval = eval_interval
+        self.eval_iters = eval_iters
+        self.batch_size = batch_size
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=learning_rate)
 
-torch.manual_seed(1337)
+        self.train_data, self.val_data = self.data.split_data()
 
-data, decode, vocab_size = load_data('input.txt')
-train_data, val_data = split_data(data)
+    @torch.no_grad()
+    def estimate_loss(self):
+        out = {}
+        self.model.eval()
+        for split in ['train', 'val']:
+            losses = torch.zeros(self.eval_iters)
+            for k in range(self.eval_iters):
+                X, Y = self.data.get_batch(split, self.batch_size, self.train_data, self.val_data)
+                _, loss = self.model(X, Y)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+        self.model.train()
+        return out
 
-model = BigramLanguageModel(vocab_size, n_embd, block_size, n_head, n_layer, dropout)
-model = model.to(device)
+    def train(self):
+        for iter in range(self.max_iters):
+            if iter % self.eval_interval == 0 or iter == self.max_iters - 1:
+                losses = self.estimate_loss()
+                print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+            #sample
+            xb, yb = self.data.get_batch('train', self.batch_size, self.train_data, self.val_data)
 
-@torch.no_grad()
-def estimate_loss():
-    model.eval()
-    losses = {split: torch.zeros(eval_iters) for split in ['train', 'val']}
-    for split, data_split in zip(['train', 'val'], [train_data, val_data]):
-        for k in range(eval_iters):
-            X, Y = get_batch(data_split, block_size, batch_size)
-            X, Y = X.to(device), Y.to(device)
-            _, loss = model(X, Y)
-            losses[split][k] = loss.item()
-    model.train()
-    return {k: v.mean() for k, v in losses.items()}
+            # Loss evaluation
+            # use of optimizer
+            _, loss = self.model(xb, yb)
+            self.optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            self.optimizer.step()
 
-for iter in range(max_iters):
-    if iter % eval_interval == 0:
-        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
-
-    xb, yb = get_batch(train_data, block_size, batch_size)
-    xb, yb = xb.to(device), yb.to(device)
-
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(model.generate(context, max_new_tokens=500, block_size=block_size)[0].tolist()))
+        # Post training
+        # final loss
+        final_losses = self.estimate_loss()
+        print(f"Final train loss {final_losses['train']:.4f}, val loss {final_losses['val']:.4f}")
