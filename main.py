6ceaa4d3e3a319cd7837920fa99d7a9b06030a16
puravrawal <purav.rawal01@student.csulb.edu>
2024-12-08 20:29:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,23 @@ n_embd = 32
 n_head = 4
 n_layer = 3
 dropout = 0.2
+
+
+torch.manual_seed(1337)
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
