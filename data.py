55b6d150fff8d79593e4744a88326e811083b97c
puravrawal <purav.rawal01@student.csulb.edu>
2024-12-09 12:43:57 -0800

    data.py

--- data.py
+++ data.py
@@ -1,24 +1,32 @@
 import torch
+#Creating class data
+class Data:
+    def __init__(self, file_path, device='cpu', block_size=256):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            self.text = f.read()
 
-def load_data(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        text = f.read()
+        self.chars = sorted(list(set(self.text)))
+        self.vocab_size = len(self.chars)
+        self.stoi = { ch:i for i,ch in enumerate(self.chars) }
+        self.itos = { i:ch for i,ch in enumerate(self.chars) }
 
-    chars = sorted(list(set(text)))
-    stoi = {ch: i for i, ch in enumerate(chars)}
-    itos = {i: ch for i, ch in enumerate(chars)}
-    encode = lambda s: [stoi[c] for c in s]
-    decode = lambda l: ''.join([itos[i] for i in l])
+        self.encode = lambda s: [self.stoi[c] for c in s]
+        self.decode = lambda l: ''.join([self.itos[i] for i in l])
 
-    data = torch.tensor(encode(text), dtype=torch.long)
-    return data, decode, len(chars)
-    
-def split_data(data, train_ratio=0.9):
-    n = int(train_ratio * len(data))
-    return data[:n], data[n:]
-
-def get_batch(data, block_size, batch_size, split='train'):
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i + block_size] for i in ix])
-    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
-    return x, y
+        self.data = torch.tensor(self.encode(self.text), dtype=torch.long)
+        self.block_size = block_size
+        self.device = device
+#splitting data
+    def split_data(self, split_ratio=0.9):
+        n = int(split_ratio * len(self.data))
+        train_data = self.data[:n]
+        val_data = self.data[n:]
+        return train_data, val_data
+#function used
+    def get_batch(self, split, batch_size, train_data, val_data):
+        data = train_data if split == 'train' else val_data
+        ix = torch.randint(len(data) - self.block_size, (batch_size,))
+        x = torch.stack([data[i:i+self.block_size] for i in ix])
+        y = torch.stack([data[i+1:i+self.block_size+1] for i in ix])
+        x, y = x.to(self.device), y.to(self.device)
+        return x, y
